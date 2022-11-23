5cc4898d6708fccabc95318d2b59ce3411e411f2
RGioele <116067942+RGioele@users.noreply.github.com>
2022-11-23 18:35:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+      int n=0,a=1, b=1;
+    cin >> n;
+    if(n>=2) {
+        cout << a << endl;
+        for (int i = 0; i < n ; i++) {
+            int c = a;
+            a += b;
+            b = c;
+            cout << c << endl;
+        }
+    }else{
+        cout << "errore"<<endl;
+    }
    return 0;
 }
